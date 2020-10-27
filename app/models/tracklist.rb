51bc4ff732e981573789e042a97eaa95e306073a
Ryan Flynn <ryanflynn44@gmail.com>
2020-10-27 11:59:29 -0400

    Updated the models relationships

--- app/models/tracklist.rb
+++ app/models/tracklist.rb
@@ -1,3 +1,8 @@
 class Tracklist < ApplicationRecord
   belongs_to :user
+  belongs_to :artist
+  belongs_to :festival
+  has_many :songs
+  has_many :likes
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
 
   has_secure_password
+  has_many :tracklists
+  has_many :verifications
+  has_many :likes
 
 
 end
